e7683f3cce3d99cb1c3c01f7b8edf4533db01d04
jhon5530 <36444655+jhon5530@users.noreply.github.com>
2026-01-08 10:57:12 -0500

    Prueba inicial de funcionamiento

---
+++ src/tendermod/config/settings.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Cargar .env desde la raíz del proyecto
+ROOT_DIR = Path(__file__).resolve().parents[3]
+load_dotenv(ROOT_DIR / ".env")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+#CHROMA_PERSIST_DIR = os.getenv("CHROMA_PERSIST_DIR", "./data/chroma")
+ENV = os.getenv("ENV", "local")
---
+++ src/tendermod/main.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+from openai import OpenAI
+from tendermod.config.settings import OPENAI_API_KEY
+
+
+
+def main():
+    print("\ntendermod running")
+    print("API KEY loaded:", bool(OPENAI_API_KEY))
+    test_openai()
+
+
+def test_openai():
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": "Hello, how are you?"}]
+    )
+    print(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
