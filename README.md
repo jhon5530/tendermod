4b1e168ae756ebb5f35a4dcfa34b5ec53858c57d
jhon5530 <36444655+jhon5530@users.noreply.github.com>
2026-01-17 11:25:24 -0500

    Antes de separar ingestion de consulta

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Actividades ejecutadas
+- (08-01-25) Se desplego el proyecto con BPS (poetry + github (local + cloud rep) + dotenv) y se realizo prueba de funcionamiento de la correcta importacion de los modulos.
+- (12-01-25) Se movio todo el codigo de indicadores para visual code
+
+Proximos pasos
+- Probar con los diferentes PDFs el correcto funcionamiento, tener en cuenta si se esta guardando en cache, probar antes de pasar al siguiente
+- Repetir este paso y ahora desplegar el caso de uso de experiencia.
+- Una vez se tengan ambos casos operativos se deben afinar (Ej. Indicadores debe evaluar todos en una sola evaluacion) para que entreguen ls datos necesarios y guarden en disco la info para retomarla mas adelante.
+- Generar una version 1 de una interfaz grafica que permita:
+   - Cargar los archivos de la licitacion
+   - Ejecutar el agente para que interpreta y devuelva los resultados esperados.
+   - Dar formato a esa respuesta.
--- src/tendermod/main.py
+++ src/tendermod/main.py
@@ -1,14 +1,27 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from openai import OpenAI
 from tendermod.config.settings import OPENAI_API_KEY
-
+from tendermod.ingestion.pdf_loader import load_docs
+from tendermod.ingestion.chunking import chunk_docs
+from tendermod.evaluation.indicators_compliance import evaluate_indicators_compliance
+from tendermod.config.settings import CHROMA_PERSIST_DIR
+from pathlib import Path
+import shutil
 
 
 def main():
     print("\ntendermod running")
-    print("API KEY loaded:", bool(OPENAI_API_KEY))
-    test_openai()
 
+    #test_openai()
+    indicators_routine()
+
+
+def indicators_routine():
+    user_input = "Cuales los indicadores financieros como: Rentabilidades, capacidades, endeudamiento, indices"
+    k = 5
+    response = evaluate_indicators_compliance(user_input=user_input, k=k)
+    print(f"Respuesta final:\n {response} \n")
+    
 
 def test_openai():
     client = OpenAI(api_key=OPENAI_API_KEY)
